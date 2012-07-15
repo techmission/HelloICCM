07fab3d8b6a8fc6b586ebfb7005b7e1558497d80
Jim Gummeson <jim@jim-Lenovo-V570.(none)>
2012-07-15 08:43:01 -0300

    step 1

--- src/com/example/helloiccm/MainActivity.java
+++ src/com/example/helloiccm/MainActivity.java
@@ -10,16 +10,11 @@ public class MainActivity extends DroidGap {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Make app full screen (no status bar)
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN | 
-        WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+
         
         super.loadUrl("file:///android_asset/www/index.html");
         
-        // We shouldn't have to do this!
-        this.appView.setInitialScale(100);
+
     }
 
     @Override
