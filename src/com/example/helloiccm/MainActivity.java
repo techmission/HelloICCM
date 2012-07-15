66a5778f76f73cb76dc9c85a60a5b01c583efebe
Jim Gummeson <jim@techmission.org>
2012-07-14 22:37:58 -0400

    -fixed viewport -got rid of status bar

--- src/com/example/helloiccm/MainActivity.java
+++ src/com/example/helloiccm/MainActivity.java
@@ -1,25 +1,30 @@
 package com.example.helloiccm;
 
 import android.os.Bundle;
-import android.app.Activity;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
 import org.apache.cordova.*;
+import android.view.WindowManager;
 
 public class MainActivity extends DroidGap {
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Make app full screen (no status bar)
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN | 
+        WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+        
         super.loadUrl("file:///android_asset/www/index.html");
+        
+        // We shouldn't have to do this!
+        this.appView.setInitialScale(100);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
-    }
-
-    
+    }  
 }
